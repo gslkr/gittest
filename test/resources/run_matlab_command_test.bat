b5a02296a95044cd9d9007d953790d696a9177fb
dyq <123@cc.com>
2022-08-02 16:56:40 +0800

    dele mat

--- test/resources/run_matlab_command_test.bat
+++
@@ -1,9 +0,0 @@
-rem Copyright 2020 The MathWorks, Inc.
-
-echo "tester_started"
-
-set "arg1=%~1"
-
-echo "%arg1%"
-
-if "%arg1%" == "exitMatlab" (exit /b 1)
--- test/resources/run_matlab_command_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Copyright 2020 The MathWorks, Inc.
-
-echo "tester_started"
-echo $1
-if [[ $1 == "exitMatlab" ]]; then
-  exit 1
-fi
